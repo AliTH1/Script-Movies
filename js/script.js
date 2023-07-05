59416c8d496eb7354f88218f105119477b5b066e
Ali Taghizada <alitaqizade2@gmail.com>
2023-07-05 14:40:07 +0400

    fixV2

--- js/script.js
+++ js/script.js
@@ -13,7 +13,11 @@ const personalMovieDB = {
 let lastWatchedMovie = prompt("Один из последних просмотренных фильмов?", ""),
 	ratingLastWatchedMovie = +prompt("На сколько оцените его?", "");
 
+let lastWatchedMovie2 = prompt("Один из последних просмотренных фильмов?", ""),
+	ratingLastWatchedMovie2 = +prompt("На сколько оцените его?", "");
 
 personalMovieDB.movies[lastWatchedMovie] = ratingLastWatchedMovie;
+personalMovieDB.movies[lastWatchedMovie2] = ratingLastWatchedMovie2;
+
 
 console.log(personalMovieDB);
